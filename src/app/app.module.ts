0e58a2df36c851f5c8114ad5916686bdc0a3c92a
thhmoc67 <dayalsaini67@gmail.com>
2018-01-10 19:07:35 +0530

    just started project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialLinksComponent,
+    AboutMeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
